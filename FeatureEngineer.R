441cc121c68dc4f43320f3bba28a5924fd2001d3
Jinmei Zhang <jinmeizhang@cd-it-6983753.dhcp.lbl.gov>
2017-06-23 15:33:11 -0700

    added more codes to feature engineering

--- FeatureEngineer.R
+++ FeatureEngineer.R
@@ -18,12 +18,32 @@ combi <- rbind(train,test)
 # combi$Name <- as.character(combi$Name)
 # combi$Name[1]
 
+## Fearther: passenger’s title
 strsplit(combi$Name[1], split='[,.]')[[1]][2] #  []: regular expressions
 
 combi$Title <- sapply(combi$Name, FUN = function(x) {strsplit(x, split='[,.]')[[1]][2]})
 # delete space at the begining of Titles
 combi$Title <-sub(' ', '', combi$Title)
 
-# %in% checks to see if a value is part of the vector
+table(combi$Title)
 
+# combine Mme and Mlle since they are similar
+#combi$PassengerId[combi$Title %in% c('Mme', 'Mlle')]
 combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle' 
+#combi$PassengerId[combi$Title %in% c('Mlle')]
+##%in%: element-wise logical OR operator
+##c(): combine values into a vector or list
+
+# combine rich men and women
+#combi$PassengerId[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')]
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+#combi$PassengerId[combi$Title %in% 'Sir']
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+
+## Feather: family size
+# add the number of siblings, spouses, parents and children
+combi$FamilySize <- combi$SibSp + combi$Parch + 1
+# add family names
+combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+# combine family names with sizes
+combi$FamilyID <- paste(combi$FamilySize, combi$Surname, sep="")
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # R_projects
 R scripts for various exercises/projects
 * iris.R computes the average petal length for each species in the order of setosa, versicolor, virginica (CDIPS project)
-* titanic.R is a machine learning exercise following Trevor Stephens's tutorial (http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/)
+* titanic.R, titanic_decisiontree.R, and FeatureEngineer.R are scripts for machine learning exercises, which  follow Trevor Stephens's tutorial (http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/)
