7c14b1cdb0666abd222dc6304d8f7673b5bb9f16
Jinmei Zhang <jmzhang@vt.edu>
2017-06-23 17:34:45 -0700

    more feature engineering

--- FeatureEngineer.R
+++ FeatureEngineer.R
@@ -1,4 +1,6 @@
 library(readr)
+library(rpart) 
+library(rpart.plot)
 
 # set working directory
 setwd("~/Documents/Projects/R_projects")
@@ -18,13 +20,11 @@ combi <- rbind(train,test)
 # combi$Name <- as.character(combi$Name)
 # combi$Name[1]
 
-## Fearther: passenger’s title
+## Feature 1: passenger’s title
 strsplit(combi$Name[1], split='[,.]')[[1]][2] #  []: regular expressions
-
 combi$Title <- sapply(combi$Name, FUN = function(x) {strsplit(x, split='[,.]')[[1]][2]})
 # delete space at the begining of Titles
 combi$Title <-sub(' ', '', combi$Title)
-
 table(combi$Title)
 
 # combine Mme and Mlle since they are similar
@@ -40,10 +40,41 @@ combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
 #combi$PassengerId[combi$Title %in% 'Sir']
 combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
 
-## Feather: family size
+## Feature 2: family size
 # add the number of siblings, spouses, parents and children
 combi$FamilySize <- combi$SibSp + combi$Parch + 1
 # add family names
 combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
 # combine family names with sizes
 combi$FamilyID <- paste(combi$FamilySize, combi$Surname, sep="")
+
+# combine the IDs for small families
+combi$FamilyID[combi$FamilySize < 3] <- 'Small'
+table(combi$FamilyID) # some small families slip through the trimming
+
+# convert table to a data frame and trim the small families
+famIDs <- data.frame(table(combi$FamilyID))
+# only keep the ones with small family size
+famIDs <- famIDs[famIDs$Freq <= 2,]
+
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
+# convert FamilyID to factor
+# factors are categorical variables which take on a limited number of different values;
+# categorical variables enter insures the correct process of data by statistical modeling functions
+combi$FamilyID <- factor(combi$FamilyID)
+
+# split the data in combi back to train and test data
+train_new <- combi[1:891,]
+test_new <- combi[892:1309,]
+# test factor
+table(combi$FamilyID)
+table(combi$train_new)
+table(combi$test_new)
+
+# generate survival information
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+             data=train_new,
+             method="class")
+# plot the decision tree
+# plot(fit) # text(fit)
+rpart.plot(fit)
